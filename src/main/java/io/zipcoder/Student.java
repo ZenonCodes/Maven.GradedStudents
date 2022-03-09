3ffa3737e53b600c584c1792c5e72784462da337
Vera <vera@Zipcoders-MBP.fios-router.home>
2022-03-08 21:03:00 -0500

    Done up to getExamScores test

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,73 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Student {
+    String firstName;
+    String lastName;
+    Double[] examScores;
+    Integer examsTaken;
+
+    public String getExamScores(){
+       int i = 1;
+       String returnString = "Exam Scores:";
+        for (int j = 0; j < examScores.length; j++){
+           returnString += ("\n\tExam " + i + " -> " + examScores[j]);
+           i++;
+        }
+        return returnString;
+    }
+
+    public Student(String firstName, String lastName, Double ... scores)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = scores;
+
+//        for(Double score : scores){
+//            this.scores.add(score);
+//        }
+
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setFullName (String first, String last){
+        this.firstName = first;
+        this.lastName = last;
+    }
+    public String getFullName(){
+        return (firstName + " " + lastName);
+    }
+    public void setExamScores(Double ... grades){
+        ArrayList<Double> mutateGrades = new ArrayList<Double>(Arrays.asList(grades));
+        mutateGrades.clear();
+        for (Double grade : grades){
+            mutateGrades.add(grade);
+        }
+        examScores = mutateGrades.toArray(examScores);
+    }
+//    public Double[] getExamScores(){
+//        return examScores;
+//    }
+    public String printScores (){
+        String print = (firstName + " " + lastName + ", you have earned the following grades so far this quarter: ");
+        for(Double score : examScores){
+            print += (score + " ");
+        }
+        return print;
+    }
+
+
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,82 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StudentTest {
 
+//    @Test
+//    public void testSetScores() {
+//        //Given
+//        Student dave = new Student("Dave", "Krillton");
+//        //When
+//        Double[] expectedScores = {97.0,93.2,65.71,81.4};
+//        dave.setScores(expectedScores);
+//        Double[] actualScores = dave.getScores();
+//
+//        //Then
+//        Assert.assertArrayEquals(actualScores, expectedScores);
+//    }
+    @Test
+    public void testPrintScores() {
+        //Given
+        Student dave = new Student("Dave", "Krillton");
+
+        //When
+        dave.setExamScores(97.0,93.2,65.71,81.4);
+        String actualPrint = dave.printScores();
+        String expectedPrint = (dave.firstName + " " + dave.lastName + ", you have earned the following grades so far this quarter: ");
+        for(Double score: dave.examScores) {
+                  expectedPrint += (score + " ");
+              }
+        //Then
+    Assert.assertEquals(expectedPrint, actualPrint);
+    }
+
+    @Test
+    public void getExamScores() {
+        // Given
+        String firstName = "Dave";
+        String lastName = "Krillton";
+        Double[] examScores = {97.0,93.2,65.71,81.4};
+        Student student = new Student(firstName, lastName, examScores);
+
+// When
+        String output = student.getExamScores();
+
+// Then
+        System.out.println(output);
+    }
+
+    @Test
+    public void setFirstName() {
+    }
+
+    @Test
+    public void getFirstName() {
+    }
+
+    @Test
+    public void setLastName() {
+    }
+
+    @Test
+    public void getLastName() {
+    }
+
+    @Test
+    public void setFullName() {
+    }
+
+    @Test
+    public void getFullName() {
+    }
+
+    @Test
+    public void setExamScores() {
+    }
+
+    @Test
+    public void printScores() {
+    }
 }
