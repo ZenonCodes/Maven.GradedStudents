4d31feb65585117862119f453245f4bf8686d4df
Vera <vera@Zipcoders-MBP.fios-router.home>
2022-03-08 22:11:43 -0500

    Added addExamScore and refined getExamScore

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -7,27 +7,15 @@ public class Student {
     String firstName;
     String lastName;
     Double[] examScores;
-    Integer examsTaken;
-
-    public String getExamScores(){
-       int i = 1;
-       String returnString = "Exam Scores:";
-        for (int j = 0; j < examScores.length; j++){
-           returnString += ("\n\tExam " + i + " -> " + examScores[j]);
-           i++;
-        }
-        return returnString;
-    }
-
+    Integer examsTaken = 0;
     public Student(String firstName, String lastName, Double ... scores)
     {
         this.firstName = firstName;
         this.lastName = lastName;
         this.examScores = scores;
-
-//        for(Double score : scores){
-//            this.scores.add(score);
-//        }
+        if(examScores.length > 0) {
+            examsTaken = examScores.length;
+        }
 
     }
     public void setFirstName(String firstName){
@@ -43,24 +31,36 @@ public class Student {
         return lastName;
     }
 
-    public void setFullName (String first, String last){
-        this.firstName = first;
-        this.lastName = last;
+    public String getExamScores() {
+        int i = 1;
+        if (examScores.length > 0) {
+            String returnString = "Exam Score(s):";
+            for (int j = 0; j < examScores.length; j++) {
+                returnString += ("\n\tExam " + i + " -> " + examScores[j]);
+                i++;
+            }
+            return returnString;
+        }else return "You haven't taken any exams yet.";
     }
-    public String getFullName(){
-        return (firstName + " " + lastName);
+    public void addExamScore(Double grade){
+        ArrayList<Double> mutateGrades = new ArrayList<Double>(Arrays.asList(examScores));
+        mutateGrades.add(grade);
+        this.examScores = mutateGrades.toArray(examScores);
+        this.examsTaken += 1;
     }
+
+
+    //Play Methods
     public void setExamScores(Double ... grades){
         ArrayList<Double> mutateGrades = new ArrayList<Double>(Arrays.asList(grades));
         mutateGrades.clear();
         for (Double grade : grades){
             mutateGrades.add(grade);
+            examsTaken += 1;
         }
         examScores = mutateGrades.toArray(examScores);
     }
-//    public Double[] getExamScores(){
-//        return examScores;
-//    }
+
     public String printScores (){
         String print = (firstName + " " + lastName + ", you have earned the following grades so far this quarter: ");
         for(Double score : examScores){
@@ -68,6 +68,19 @@ public class Student {
         }
         return print;
     }
-
+    public void addExamScores(Double ... grades){
+        ArrayList<Double> mutateGrades = new ArrayList<Double>(Arrays.asList(grades));
+        for (Double grade : grades){
+            mutateGrades.add(grade);
+        }
+        examScores = mutateGrades.toArray(examScores);
+    }
+    public void setFullName (String first, String last){
+        this.firstName = first;
+        this.lastName = last;
+    }
+    public String getFullName(){
+        return (firstName + " " + lastName);
+    }
 
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -2,36 +2,9 @@ package io.zipcoder;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 
 public class StudentTest {
-
-//    @Test
-//    public void testSetScores() {
-//        //Given
-//        Student dave = new Student("Dave", "Krillton");
-//        //When
-//        Double[] expectedScores = {97.0,93.2,65.71,81.4};
-//        dave.setScores(expectedScores);
-//        Double[] actualScores = dave.getScores();
-//
-//        //Then
-//        Assert.assertArrayEquals(actualScores, expectedScores);
-//    }
-    @Test
-    public void testPrintScores() {
-        //Given
-        Student dave = new Student("Dave", "Krillton");
-
-        //When
-        dave.setExamScores(97.0,93.2,65.71,81.4);
-        String actualPrint = dave.printScores();
-        String expectedPrint = (dave.firstName + " " + dave.lastName + ", you have earned the following grades so far this quarter: ");
-        for(Double score: dave.examScores) {
-                  expectedPrint += (score + " ");
-              }
-        //Then
-    Assert.assertEquals(expectedPrint, actualPrint);
-    }
 
     @Test
     public void getExamScores() {
@@ -40,6 +13,21 @@ public class StudentTest {
         String lastName = "Krillton";
         Double[] examScores = {97.0,93.2,65.71,81.4};
         Student student = new Student(firstName, lastName, examScores);
+
+// When
+        String output = student.getExamScores();
+
+// Then
+        System.out.println(output);
+    }
+    @Test
+    @DisplayName("Tests getExamScores when score array is empty ☺")
+    public void getExamScoresEmpty() {
+        // Given
+        String firstName = "Dave";
+        String lastName = "Krillton";
+
+        Student student = new Student(firstName, lastName);
 
 // When
         String output = student.getExamScores();
@@ -73,10 +61,58 @@ public class StudentTest {
     }
 
     @Test
+    @DisplayName("Tests the ability to set multiple scores")
     public void setExamScores() {
     }
 
     @Test
     public void printScores() {
     }
+
+    @Test
+    public void addExamScore() {
+        //Given
+        String firstName = "Dave";
+        String lastName = "Krillton";
+        Student student = new Student(firstName,lastName);
+
+        //When
+        student.addExamScore(88.4);
+        String output = student.getExamScores();
+        //Then
+        System.out.println(output);
+    }
+
+    @Test
+    @DisplayName("Add multiple score to non-empty array")
+    public void addExamScores() {
+    }
 }
+
+//    @Test
+//    public void testSetScores() {
+//        //Given
+//        Student dave = new Student("Dave", "Krillton");
+//        //When
+//        Double[] expectedScores = {97.0,93.2,65.71,81.4};
+//        dave.setScores(expectedScores);
+//        Double[] actualScores = dave.getScores();
+//
+//        //Then
+//        Assert.assertArrayEquals(actualScores, expectedScores);
+//    }
+//@Test
+//public void testPrintScores() {
+//    //Given
+//    Student student = new Student("Dave", "Krillton");
+//
+//    //When
+//    student.setExamScores(97.0,93.2,65.71,81.4);
+//    String actualPrint = student.printScores();
+//    String expectedPrint = (student.firstName + " " + student.lastName + ", you have earned the following grades so far this quarter: ");
+//    for(Double score: student.examScores) {
+//        expectedPrint += (score + " ");
+//    }
+//    //Then
+//    Assert.assertEquals(expectedPrint, actualPrint);
+//}
